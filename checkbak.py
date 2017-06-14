8aa5abd6d519eb2cf11bb40a10b2d0640fbaefc6
Plaoo <paolo.monni345@gmail.com>
2017-06-14 20:34:38 +0200

    for per zip

--- checkbak.py
+++ checkbak.py
@@ -3,9 +3,9 @@ import os
 from pathlib import Path
 #for python 3.5
 directory = os.fsencode(os.curdir)
-pathlist = Path(os.curdir).glob('**/*.zip')
-for path in pathlist:
+pathlist = Path(os.curdir)
 
+for path in pathlist.glob('**/*.zip'):
         archive = zipfile.ZipFile(str(path))
         er = archive.testzip()
         if(archive.testzip() != None):
