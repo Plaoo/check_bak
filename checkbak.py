31fc3bbc485e019e8b23bd11f73dd75c3e3f1303
Plaoo <paolo.monni345@gmail.com>
2017-06-15 22:45:47 +0200

    add check7z

--- checkbak.py
+++ checkbak.py
@@ -1,16 +1,32 @@
 import zipfile
 import os
+import rarfile
+import lzma
+
 from pathlib import Path
-#for python 3.5
+
+# for python 3.5
 directory = os.fsencode(os.curdir)
 pathlist = Path(os.curdir)
 
+
+# controllo gli zip in modo ricursivo
 def checkzip(path):
     for path in pathlist.glob('**/*.zip'):
-            archive = zipfile.ZipFile(str(path))
-            er = archive.testzip()
-            if(archive.testzip() != None):
-                print(path, er)
+        archive = zipfile.ZipFile(str(path))
+        er = archive.testzip()
+        if (archive.testzip() != None):
+            print(path, er)
+
+
+# controllo i rar in modo ricursivo
+def check7z(path):
+    for path in pathlist.glob('**/*.7z'):
+        archive = lzma.open(str(path), "r")
+
+
+    pass
+
 
 if __name__ == "__main__":
     checkzip(pathlist)
